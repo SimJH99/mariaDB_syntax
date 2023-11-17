0ec66319712e8dbed7ae7998beda5becf0d103c7
SimJH99 <s99s4481@gmail.com>
2023-11-17 10:57:17 +0900

    2023-11-17 10:57

--- basic/3.DML.sql
+++ basic/3.DML.sql
@@ -27,3 +27,8 @@ SELECT * FROM author ORDER BY name, email desc;
 
 -- LIMIT NUMBER : 특정숫자로 결과값 개수 제한
 SELECT * FROM author ORDER BY id DESC LIMIT 2;
+
+-- ALIAS를 이용한 SELECT문 /as는 생략이 가능
+SELECT name AS 이름 FROM author;
+-- AS는 생략 가능
+SELECT name, email FROM author AS a;
