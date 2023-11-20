b2d75cd2bac641ca686841fdc312106af9deb014
SimJH99 <s99s4481@gmail.com>
2023-11-20 11:53:04 +0900

    2023-11-20 11:52

--- basic/2.DATA_TYPE/data_type_practice5.sql
+++ basic/2.DATA_TYPE/data_type_practice5.sql
@@ -6,4 +6,11 @@ SELECT * FROM post WHERE created_time BETWEEN '2023-01-01' AND '2023-12-31';
 SELECT * FROM post WHERE created-time >= '2023-01-01' AND created_time <= '2023-12-31';
 
 -- - 실습2: now()를 활용해서 오늘날짜에 생성된 데이터 출력하기
-SELECT * from post WHERE created_time = now();
+SELECT * FROM post WHERE created_time = NOW();
+SELECT * FROM post WHERE DATE_FORMAT(created_time, '%Y-%m-%d'); = DATE_FORMAT(NOW(), '%Y-%m-%d');
+
+-- 실습3: ALTER문을 써서 post의 title을 not null조건으로 바꿔보자
+ALTER TABLE post MODIFY COLUMN title VARCHAR(255) NOT NULL;
+
+--실습4 : author, post테이블의 is에 auto_increment로 바꿔보자
+ALTER TABLE post MODIFY COLUMN id INT auto_increment;
