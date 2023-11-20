5cfc2d8097a27383256cb9d2abedad05eba91ad2
SimJH99 <s99s4481@gmail.com>
2023-11-20 10:57:32 +0900

    2023-11-20 10:57

--- basic/2.DATA_TYPE/data_type_practice5.sql
+++ basic/2.DATA_TYPE/data_type_practice5.sql
@@ -2,8 +2,8 @@
 
 SELECT DATE_FROMAT(created_time, '%Y-%m-%d') AS created_time FROM post;
 SELECT * FROM post WHERE created_time LIKE '2023%';
-SELECT * FROM post WHERE created_time BETWEEN '2023-01-01' AND '2023-11-17';
-SELECT * FROM post WHERE created-time >= '2023-01-01' AND created_time <= '2023-11-17';
+SELECT * FROM post WHERE created_time BETWEEN '2023-01-01' AND '2023-12-31';
+SELECT * FROM post WHERE created-time >= '2023-01-01' AND created_time <= '2023-12-31';
 
 -- - 실습2: now()를 활용해서 오늘날짜에 생성된 데이터 출력하기
 SELECT * from post WHERE created_time = now();
