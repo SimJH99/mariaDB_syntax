9c7eaaa69e92c9b3b63f4b73ff0ffdc4cf40c7c1
Simguri <s99s448@gmail.com>
2023-11-20 21:48:20 +0900

    2023-11-20 21:48

--- basic/3.CONSTRAINT/constraint_practice3.sql
+++ basic/3.CONSTRAINT/constraint_practice3.sql
@@ -1,18 +1,20 @@
 -- ON DELETE CASCADE 실습
--- -외래키 제약 조건에서 ON DELETE CASCADE등의 옵션
--- - ON DELETE CASCADE 테스트
---   - 삭제 시
---     - author 삭제시 post 같이 삭제
---   - 수정 시
---     - author id 수정 불가
---     - restrict와 동일
+-- 외래키 제약 조건에서 ON DELETE CASCADE등의 옵션
+-- ON DELETE CASCADE 테스트
+
+-- 삭제 시
+-- author 삭제시 post 같이 삭제
+
+-- 수정 시
+-- author id 수정 불가
+-- restrict와 동일
     
--- - ON DELETE SET NULL, ON UPDATE SET NULL 또한 동일한 방법으로 테스트
+-- ON DELETE SET NULL, ON UPDATE SET NULL 또한 동일한 방법으로 테스트
 
 
 -- 기존 fk, 인덱스 삭제
 alter table post drop index post_author_fk;
 alter table post drop index author_id;
 
--- 
-ALTER TABLE POST ADD CONSTRAINT post_author_fk FOREIGN KEY(author_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE SET NULL;
+-- post테이블에 새로운 fk 생성 및 설정
+ALTER TABLE post ADD CONSTRAINT post_author_fk FOREIGN KEY(author_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE SET NULL;
--- basic/FLOWCONTROL/flowcontrol_practice3.sql
+++ basic/FLOWCONTROL/flowcontrol_practice3.sql
@@ -1,15 +1,12 @@
 -- post 테이블에서 id,title,contents, 그리고 author_id의 경우 author_type이라는 이름으로 조회,
 -- author_id가 만약 1이면 first_author, 2이면 second_author로 조회가 되도록 하고 
 -- 3이상 그외의 경우 etc_authors로 조회도록 하여라.
--- - 1.case when 사용
--- - 2.if문 사용하여 동일하게 출력
+-- 1.case when 사용
+-- 2.if문 사용하여 동일하게 출력
 
--- - 프로그래머스 sql문제 풀이(case when) - 조건에 부합하는 중고거래 상태 조회하기
--- -iffull을 사용하여 만약 contesnts가 null로 비어있으면 no contesnts로 출력 되도록 select문 작성
+-- iffull을 사용하여 만약 contesnts가 null로 비어있으면 no contesnts로 출력 되도록 select문 작성
 
--- - 프로그래머스 sql 문제풀이 (흐름제어(IF, IFNULL) - 12세 이하인 여자 환자 목록 출력하기)
-
-1.
+--1. CASE WHEN 문
 select id, title, contents, 
 case author_id 
 	when 1 then 'First_author'
@@ -19,15 +16,14 @@ case author_id
 end as author_type
 from post;
 
-2.
+-- 2. IF문
 select id, title, contents,
 if(author_id = 1, 'First_author',if(author_id = 2, 'Second_author', if(author_id is null, 'anonymous', 'Etc_author'))) 
 as author_type
 from post;
 
 
-
---if 문법 사용해서 만약 author_id가 1이면 first author 그렇지 않은 경우엔 others 출력하시오
+-- 3. if 문법 사용해서 만약 author_id가 1이면 first author 그렇지 않은 경우엔 others 출력하시오
 -- id, title, contents, author_type 형식으로 출력
 select id, title, contents,
 if(author_id = 1, 'first author', 'others') 
