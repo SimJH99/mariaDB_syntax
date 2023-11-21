c5f61fda94450f06b05d2d3585f1cf88395a9baf
SimJH99 <s99s4481@gmail.com>
2023-11-21 14:34:35 +0900

    2023-11-21 14:34

--- basic/5.JOIN/join_practice.3.sql
+++ basic/5.JOIN/join_practice.3.sql
@@ -2,3 +2,13 @@
 -- post 테이블 title, contents 정보를 UNION 하여 조회
 
 SELECT 컬럼1, 컬럼2 FROM TABLE1 UNION SELECT 컬럼1, 컬럼2 FROM TABLE2;
+
+SELECT name, email FROM author UNION SELECT title, contents FROM post;
+
+
+-- group by author_id
+-- author_id 별 count 값
+-- author_id 별 price sum, avg
+
+ SELECT author_id, count(*) FROM post GROUP BY author_id;
+ SELECT author_id, sum(price) AS sum, round(avg(price)) AS avg FROM post GROUP BY author_id;
