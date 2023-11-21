a1c9f1b8c48aa2ce2898617822db25fa2bacbecd
SimJH99 <s99s4481@gmail.com>
2023-11-21 15:01:44 +0900

    2023-11-21 15:01

--- basic/5.JOIN/join_practice.3.sql
+++ basic/5.JOIN/join_practice.3.sql
@@ -9,6 +9,27 @@ SELECT name, email FROM author UNION SELECT title, contents FROM post;
 -- group by author_id
 -- author_id 별 count 값
 -- author_id 별 price sum, avg
+SELECT author_id, count(*) FROM post GROUP BY author_id;
+SELECT author_id, sum(price) AS sum, round(avg(price)) AS avg FROM post GROUP BY author_id;
 
- SELECT author_id, count(*) FROM post GROUP BY author_id;
- SELECT author_id, sum(price) AS sum, round(avg(price)) AS avg FROM post GROUP BY author_id;
+
+-- author_id 별로 price 평균값을 구하시오.
+-- 단, 건별로 2000이상인 데이터만 평균을 내서 출력하시오
+select author_id, avg(price) as avg
+from post
+where price >= 2000
+group by author_id;
+
+-- author_id 별로 price 평균값을 구하되 평균값이 2000원 이상건만 출력하시오.
+select author_id, avg(price) as avg 
+from post 
+group by author_id having avg >= 2000;
+
+
+
+-- 1번, 2번 조건을 합친 값을 출력하시오.
+select authot_id, avg(price) as avg
+from post
+where price >= 2000
+group by author_id
+having avg >= 2000;
