0399638e25a47c139502424a2a287c4a668ce40f
SimJH99 <s99s4481@gmail.com>
2023-11-22 16:12:35 +0900

    2023-11-22 16:12

--- basic/5.JOIN/view_practice.sql
+++ basic/5.JOIN/view_practice.sql
@@ -58,3 +58,22 @@ BEGIN
     END IF;
 END //
 DELIMITER ;
+
+
+-- author테이블에 while문을 활용하여 데이터 대량 insert
+-- 100건
+
+
+DELIMITER //
+CREATE PROCEDURE insert_text (IN p_title VARCHAR(255))
+BEGIN
+	DECLARE a INT default 0;
+	while a < 100 DO
+		insert into post(title) values (concat(p_title, a)) ;
+		set a = a + 1;
+	end while;
+end //
+DELIMITER ;
+
+
+call insert_text('sim');
