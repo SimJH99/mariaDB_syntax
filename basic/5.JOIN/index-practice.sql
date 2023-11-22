d12864259a68459afeb83a24237cfa3ff7bd8e84
SimJH99 <s99s4481@gmail.com>
2023-11-22 10:44:01 +0900

    2023-11-22 10:43

---
+++ basic/5.JOIN/index-practice.sql
@@ -0,0 +1,9 @@
+-- author name으로 단일 컬럼 index;
+-- select * from author where name = 'abc'
+create index name_index on author(name);
+
+
+
+--  복합컬럼: index: author의 name, email, index
+-- select * from author where name = 'abc' and email = 'xx'
+create index email_name_index on author(email, name);
