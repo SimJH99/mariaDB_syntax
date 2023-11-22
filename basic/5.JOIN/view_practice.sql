257138ce60a05271d1f590c55ae1994bc22857de
SimJH99 <s99s4481@gmail.com>
2023-11-22 14:49:57 +0900

    2023-11-22 14:49

--- basic/5.JOIN/view_practice.sql
+++ basic/5.JOIN/view_practice.sql
@@ -25,3 +25,16 @@ call getUser(5);
 
 --프로시저 내용 조회
 SHOW CREATE PROCEDURE 프로시저명;
+
+
+
+--post 테이블에 쉽게 insert 할 수 있는 post관련 프로시저 생성
+-- 사용자에게 title, contesnts, author_id 만 입력받아 insert 하는 insert문 생성
+DELIMITER //
+CREATE PROCEDURE insertPost (IN p_title varchar(255), p_contents varchar(3000), p_author_id INT)
+BEGIN
+  insert into post(title, contents, author_id) values (p_title, p_contents, p_author_id);
+END //
+DELIMITER ;
+
+call insertPost('procedure_test', 'hello guys~', 5);
